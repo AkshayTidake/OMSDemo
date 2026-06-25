d3ff2f72d33bfc1486014b26d98010a22f5e6442
AkshayTidake <akshaytidake0918@gmail.com>
2026-06-25 15:52:55 +0530

    login api created

--- app/core/security.py
+++ app/core/security.py
@@ -1,24 +1,29 @@
-from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+# from pwdlib import PasswordHash
+from passlib.context import CryptoContext
+from datetime import datetime, timedelta
+from jose import jwt
+from app.core.config import settings
 
-fake_users_db ={
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "is_active": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret",
-        "is_active": True,
-    }
-}
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def fake_hashed_password(password:str):
-    return "fakehashed" + password
+pwd_context = CryptoContext(schemes=["bycrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password,hashed_password)
+
+def create_access_token(data: dict):
+    payload = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCES_TOKEN_EXPIRE_MINUTES)
+    payload.update({"exp": expire})
+
+    return jwt.encode(payload, settings.SECRET_KEY,
+                      algorithm=settings.ALGORITHM)
+
+# password_hash = PasswordHash.recommended()
+# hashed = password_hash.hash(password)
+# password_hash.verify(password,hashed)
+
 
 
