ddb4291a07126310c6268ff11660642448dfa9b3
AkshayTidake <akshaytidake0918@gmail.com>
2026-07-03 18:10:59 +0530

    import issue fixed in database.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker, 
     AsyncSession)
-from .config import settings
+from app.core.config import settings
 from sqlalchemy.orm import DeclarativeBase
 
 
